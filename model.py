58952142e7da1273d49daaf3891617c46c3bf949
Xurshidbek003 <xurshidbekt571@gmail.com>
2024-10-30 21:11:18 +0500

    token add

--- model.py
+++ model.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255), unique=True, index=True)
-    hashed_password = Column(String(255))
-    is_active = Column(Boolean, default=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_verified = Column(Boolean, default=False)
+    verification_token = Column(String, unique=True, nullable=True)
